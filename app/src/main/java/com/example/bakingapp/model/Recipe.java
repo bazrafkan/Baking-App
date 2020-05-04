6a7aaf678613d648a21d693d647f28be9c755e06
bazrafkan <bazrafkan.mb@gmail.com>
2020-05-04 12:32:55 +0200

    feat: add recipe model

---
+++ app/src/main/java/com/example/bakingapp/model/Recipe.java
@@ -0,0 +1,75 @@
+package com.example.bakingapp.model;
+
+import java.util.List;
+
+public class Recipe {
+    public static final String baseUrl = "https://d17h27t6h515a5.cloudfront.net/";
+    public static final String pathUrl = "topher/2017/May/59121517_baking/baking.json";
+
+    private int id;
+    private String name;
+    private List<Ingredient> ingredients;
+    private List<Step> steps;
+    private int servings;
+    private String image;
+
+    public Recipe() {
+    }
+
+    public Recipe(int id, String name, List<Ingredient> ingredients, List<Step> steps, int servings, String image) {
+        this.id = id;
+        this.name = name;
+        this.ingredients = ingredients;
+        this.steps = steps;
+        this.servings = servings;
+        this.image = image;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public List<Ingredient> getIngredients() {
+        return ingredients;
+    }
+
+    public List<Step> getSteps() {
+        return steps;
+    }
+
+    public int getServings() {
+        return servings;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setIngredients(List<Ingredient> ingredients) {
+        this.ingredients = ingredients;
+    }
+
+    public void setSteps(List<Step> steps) {
+        this.steps = steps;
+    }
+
+    public void setServings(int servings) {
+        this.servings = servings;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+}
