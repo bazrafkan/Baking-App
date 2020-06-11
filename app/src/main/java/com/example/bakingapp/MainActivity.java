681d780ede148f3254a859b23d1364f849f14460
bazrafkan <bazrafkan.mb@gmail.com>
2020-06-11 22:55:32 +0200

    fix: save instance state for main activity

--- app/src/main/java/com/example/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/bakingapp/MainActivity.java
@@ -4,6 +4,7 @@ import com.example.bakingapp.adapter.RecipeAdapter;
 import com.example.bakingapp.model.Recipe;
 import com.example.bakingapp.task.RecipeTask;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,6 +13,8 @@ import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 import android.os.Bundle;
 import android.widget.Toast;
 
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity
@@ -23,18 +26,53 @@ public class MainActivity extends AppCompatActivity
     private List<Recipe> mRecipeList;
     private RecipeTask mRecipeTask;
     private RecyclerView mRecyclerView;
+    private static final String LIST_RECIPE_KEY = "recipe_list_key";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
         mSwipeRefreshLayout = findViewById(R.id.sr_recipe_list);
         mRecyclerView = findViewById(R.id.rv_recipe_list);
 
         mRecipeTask = new RecipeTask(this);
         mSwipeRefreshLayout.setOnRefreshListener(this);
+
+        if (savedInstanceState != null && savedInstanceState.containsKey(LIST_RECIPE_KEY)) {
+            Serializable serializable = savedInstanceState.getSerializable(LIST_RECIPE_KEY);
+            try {
+                mRecipeList = (ArrayList) serializable;
+                showListRecipe();
+            } catch (Exception e) {
+                getListRecipe();
+            }
+        } else {
+            getListRecipe();
+        }
+    }
+
+    private void showListRecipe() {
+        RecipeAdapter mRecipeAdapter = new RecipeAdapter(this, mRecipeList);
+        mRecipeAdapter.setOnClickListener(this);
+        mRecyclerView.setAdapter(mRecipeAdapter);
+        int list_recipe_column_item = getResources().getInteger(R.integer.list_recipe_column_item);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, list_recipe_column_item);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+    }
+
+    private void getListRecipe() {
         mSwipeRefreshLayout.setRefreshing(true);
         mRecipeTask.execute();
+    }
+
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        if (mRecipeList != null && mRecipeList.size() > 0) {
+            outState.putSerializable(LIST_RECIPE_KEY, new ArrayList<>(mRecipeList));
+        }
+        super.onSaveInstanceState(outState);
     }
 
     @Override
@@ -45,13 +83,8 @@ public class MainActivity extends AppCompatActivity
     @Override
     public void onPostExecute(List<Recipe> recipeList) {
         mRecipeList = recipeList;
-        RecipeAdapter mRecipeAdapter = new RecipeAdapter(this, mRecipeList);
-        mRecipeAdapter.setOnClickListener(this);
-        mRecyclerView.setAdapter(mRecipeAdapter);
-        int list_recipe_column_item = getResources().getInteger(R.integer.list_recipe_column_item);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, list_recipe_column_item);
-        mRecyclerView.setLayoutManager(gridLayoutManager);
         mSwipeRefreshLayout.setRefreshing(false);
+        showListRecipe();
     }
 
     @Override
