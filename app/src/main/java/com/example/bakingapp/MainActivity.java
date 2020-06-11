8fb4dd575425ee30d0428bf662a1851e16898e3c
bazrafkan <bazrafkan.mb@gmail.com>
2020-06-11 17:48:51 +0200

    feat: implement recipe list in main activity

--- app/src/main/java/com/example/bakingapp/MainActivity.java
+++ app/src/main/java/com/example/bakingapp/MainActivity.java
@@ -1,14 +1,72 @@
 package com.example.bakingapp;
 
+import com.example.bakingapp.adapter.RecipeAdapter;
+import com.example.bakingapp.model.Recipe;
+import com.example.bakingapp.task.RecipeTask;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.swiperefreshlayout.widget.SwipeRefreshLayout;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity
+        implements RecipeTask.AsyncRecipeTaskResult,
+        SwipeRefreshLayout.OnRefreshListener,
+        RecipeAdapter.RecipeAdapterOnClick {
+
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private List<Recipe> mRecipeList;
+    private RecipeTask mRecipeTask;
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mSwipeRefreshLayout = findViewById(R.id.sr_recipe_list);
+        mRecyclerView = findViewById(R.id.rv_recipe_list);
+
+        mRecipeTask = new RecipeTask(this);
+        mSwipeRefreshLayout.setOnRefreshListener(this);
+        mSwipeRefreshLayout.setRefreshing(true);
+        mRecipeTask.execute();
+    }
+
+    @Override
+    public void onPreExecute() {
+
+    }
+
+    @Override
+    public void onPostExecute(List<Recipe> recipeList) {
+        mRecipeList = recipeList;
+        RecipeAdapter mRecipeAdapter = new RecipeAdapter(this, mRecipeList);
+        mRecipeAdapter.setOnClickListener(this);
+        mRecyclerView.setAdapter(mRecipeAdapter);
+        int list_recipe_column_item = getResources().getInteger(R.integer.list_recipe_column_item);
+        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, list_recipe_column_item);
+        mRecyclerView.setLayoutManager(gridLayoutManager);
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onFailureExecute(Throwable throwable) {
+        mSwipeRefreshLayout.setRefreshing(false);
+    }
+
+    @Override
+    public void onRefresh() {
+        mRecipeTask.execute();
+    }
+
+    @Override
+    public void onClick(int position) {
+        Recipe recipe = mRecipeList.get(position);
+        Toast.makeText(this, "Recipe: " + recipe.getName(), Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/com/example/bakingapp/adapter/RecipeAdapter.java
@@ -0,0 +1,88 @@
+package com.example.bakingapp.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.bakingapp.R;
+import com.example.bakingapp.model.Recipe;
+import com.squareup.picasso.Picasso;
+
+import java.util.List;
+
+public class RecipeAdapter extends RecyclerView.Adapter<RecipeAdapter.RecipeViewHolder> {
+    public interface RecipeAdapterOnClick {
+        void onClick(int position);
+    }
+
+    private Context context;
+    private List<Recipe> mRecipeList;
+    private RecipeAdapterOnClick recipeAdapterOnClick;
+
+    public RecipeAdapter(Context context, List<Recipe> mRecipeList) {
+        this.context = context;
+        this.mRecipeList = mRecipeList;
+    }
+
+    public void setOnClickListener(RecipeAdapterOnClick recipeAdapterOnClick) {
+        this.recipeAdapterOnClick = recipeAdapterOnClick;
+    }
+
+    @NonNull
+    @Override
+    public RecipeAdapter.RecipeViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(context).inflate(R.layout.recipe_card, parent, false);
+        return new RecipeViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecipeAdapter.RecipeViewHolder holder, int position) {
+        Recipe recipe = mRecipeList.get(position);
+        String url = recipe.getImage();
+        if (url.isEmpty()) {
+            holder.mImageViewRecipe.setImageResource(R.drawable.recipe);
+        } else {
+            Picasso.get()
+                    .load(url)
+                    .centerCrop()
+                    .placeholder(R.drawable.recipe)
+                    .error(R.drawable.recipe)
+                    .into(holder.mImageViewRecipe);
+        }
+        holder.mTextViewTitle.setText(recipe.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mRecipeList != null) {
+            return mRecipeList.size();
+        } else {
+            return 0;
+        }
+    }
+
+    public class RecipeViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        ImageView mImageViewRecipe;
+        TextView mTextViewTitle;
+
+        public RecipeViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mImageViewRecipe = itemView.findViewById(R.id.iv_item_photo);
+            mTextViewTitle = itemView.findViewById(R.id.tv_item_title);
+            itemView.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if (recipeAdapterOnClick != null) {
+                recipeAdapterOnClick.onClick(getAdapterPosition());
+            }
+        }
+    }
+}
