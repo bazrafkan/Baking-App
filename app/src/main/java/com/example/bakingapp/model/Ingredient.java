ffdc5ce22016cf7227850202ef31aace2ad77269
bazrafkan <bazrafkan.mb@gmail.com>
2020-06-12 19:10:37 +0200

    fix: add serializable for models

--- app/src/main/java/com/example/bakingapp/model/Ingredient.java
+++ app/src/main/java/com/example/bakingapp/model/Ingredient.java
@@ -1,7 +1,9 @@
 package com.example.bakingapp.model;
 
 
-public class Ingredient {
+import java.io.Serializable;
+
+public class Ingredient implements Serializable {
     private float quantity;
     private String measure;
     private String ingredient;
