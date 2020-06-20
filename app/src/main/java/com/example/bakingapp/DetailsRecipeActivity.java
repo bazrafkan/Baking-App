385c2e1e438baa53fbfe98d6de95a723c883d47c
bazrafkan <bazrafkan.mb@gmail.com>
2020-06-20 13:09:49 +0200

    fix: update details activity

--- app/src/main/java/com/example/bakingapp/DetailsRecipeActivity.java
+++ app/src/main/java/com/example/bakingapp/DetailsRecipeActivity.java
@@ -5,14 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 
+import com.example.bakingapp.adapter.RecipeAdapterOnClick;
+import com.example.bakingapp.fragment.RecipeDetailsListFragment;
+import com.example.bakingapp.fragment.RecipeIngredientsListFragment;
+import com.example.bakingapp.fragment.RecipeStepDetailsFragment;
 import com.example.bakingapp.model.Recipe;
 import com.example.bakingapp.utility.Constant;
 
 import java.io.Serializable;
 
-public class DetailsRecipeActivity extends AppCompatActivity {
+public class DetailsRecipeActivity extends AppCompatActivity implements RecipeAdapterOnClick {
 
     private Recipe mRecipe;
+    private int indexRecipe;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,5 +45,59 @@ public class DetailsRecipeActivity extends AppCompatActivity {
     }
 
     private void showRecipe() {
+        RecipeDetailsListFragment recipeDetailsListFragment = RecipeDetailsListFragment
+                .newInstance(mRecipe);
+        recipeDetailsListFragment.setRecipeAdapterOnClick(this);
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.recipe_details_list_container, recipeDetailsListFragment)
+                .commit();
+    }
+
+    @Override
+    public void onClick(int position) {
+        if (position == 0) {
+            RecipeIngredientsListFragment recipeIngredientsListFragment = RecipeIngredientsListFragment
+                    .newInstance(mRecipe.getIngredients());
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.recipe_details_list_container, recipeIngredientsListFragment)
+                    .commit();
+
+        } else {
+            indexRecipe = position - 1;
+            final RecipeStepDetailsFragment recipeStepDetailsFragment = RecipeStepDetailsFragment
+                    .newInstance(mRecipe.getSteps().get(indexRecipe));
+            checkButtonVisibility(recipeStepDetailsFragment);
+            recipeStepDetailsFragment.setRecipeStepClick(new RecipeStepDetailsFragment.RecipeStepClick() {
+                @Override
+                public void onNextClick() {
+                    indexRecipe++;
+                    checkButtonVisibility(recipeStepDetailsFragment);
+                    recipeStepDetailsFragment.setStep(mRecipe.getSteps().get(indexRecipe));
+                }
+
+                @Override
+                public void onPreviousClick() {
+                    indexRecipe--;
+                    checkButtonVisibility(recipeStepDetailsFragment);
+                    recipeStepDetailsFragment.setStep(mRecipe.getSteps().get(indexRecipe));
+                }
+            });
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.recipe_details_list_container, recipeStepDetailsFragment)
+                    .commit();
+        }
+    }
+
+    private void checkButtonVisibility(RecipeStepDetailsFragment recipeStepDetailsFragment) {
+        if (indexRecipe > 0) {
+            recipeStepDetailsFragment.setVisiblePrevious(true);
+        } else {
+            recipeStepDetailsFragment.setVisiblePrevious(false);
+        }
+        if (indexRecipe < mRecipe.getSteps().size() - 2) {
+            recipeStepDetailsFragment.setVisibleNext(true);
+        } else {
+            recipeStepDetailsFragment.setVisibleNext(false);
+        }
     }
 }
