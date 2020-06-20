50e8af69809ada31cd0bde56be5b6af4ffe6cd77
bazrafkan <bazrafkan.mb@gmail.com>
2020-06-20 12:59:40 +0200

    fix: add constants strings

--- app/src/main/java/com/example/bakingapp/utility/Constant.java
+++ app/src/main/java/com/example/bakingapp/utility/Constant.java
@@ -1,8 +1,12 @@
 package com.example.bakingapp.utility;
 
 public class Constant {
-    public static final String LIST_RECIPE_KEY = "recipe_list_key";
-    public static final String SELECTED_RECIPE_KEY = "selected_recipe_key";
     public static final String BASE_URL = "https://d17h27t6h515a5.cloudfront.net/";
     public static final String PATH_URL = "topher/2017/May/59121517_baking/baking.json";
+
+    public static final String LIST_RECIPE_KEY = "recipe_list_key";
+    public static final String SELECTED_RECIPE_KEY = "selected_recipe_key";
+    public static final String RECIPE_DETAILS_KEY = "recipe_details_key";
+    public static final String RECIPE_INGREDIENTS_KEY = "recipe_ingredients_key";
+    public static final String RECIPE_STEP_KEY = "recipe_step_key";
 }
